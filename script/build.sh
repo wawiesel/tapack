028f2dfefcaa5c64a399d848e7f7013f7e6b7cb0
William A. Wieselquist <william.wieselquist@gmail.com>
2012-09-24 20:29:36 -0400

    modified version

--- script/build.sh
+++ script/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+nprocs=$(grep '^processor' /proc/cpuinfo | wc -l)
+
 #check for makefile and regenerate if doesn't exist
 if [ -e "build/Makefile" ];
 then
     echo "*** building with existing build/Makefile ***"
-    cd build && make
+    cd build && make -j$nprocs
 else
     echo "*** creating new makefile ***"
-    script/config.sh $1 && cd build && make
+    script/config.sh $1 && cd build && make -j$nprocs
 fi
